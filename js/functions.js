7190b8475b40c568f005cf8df51feb294c55b0e6
Ching Choi <chingchoi128@gmail.com>
2018-04-29 16:05:07 -0700

    Interest section polished

--- js/functions.js
+++ js/functions.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
             // Only prevent default if animation is actually gonna happen
             event.preventDefault();
             $('html, body').animate({
-            scrollTop: target.offset().top - 40
+            scrollTop: target.offset().top - 39
             }, 1000, function() {
             // Callback after animation
             // Must change focus!
